63d35d66121064831e167b3cf2f19b1d27c09ee4
Bree2003 <breeale2003@gmail.com>
2024-03-22 18:37:52 -0300

    trying images view

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      loader: 'custom',
-      loaderFile: './my-loader.ts',
-    },
+  output: 'export',
+  images: { unoptimized: true },
   }
    
   module.exports = nextConfig
